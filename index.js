fde7578ade3bd4983b7c1ce6d380ced8c2740431
Veronica <veronicapadon@gmail.com>
2024-04-01 13:10:43 -0400

    updated

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.use(cookieParser());
 
 app.use("/api/v1", routes);
 
+app.get("/", (req, res) => {
+   res.send("mernProject") ;
+})
+
 const port = process.env.PORT || 5000;
 
 const server =  http.createServer(app);
